0ab07d7aa97be801bfed570e430599761cc5eea6
koyeo <koyeo@qq.com>
2023-02-01 09:48:26 +0800

    add error utils

--- types.go
+++ types.go
@@ -1,5 +1,21 @@
 package mix
 
-import "github.com/gozelle/jsonrpc"
+import (
+	"fmt"
+	"github.com/gozelle/jsonrpc"
+)
 
 type Error = jsonrpc.Error
+
+func Errorf(format string, a ...any) Error {
+	return Error{
+		Message: fmt.Sprintf(format, a...),
+	}
+}
+
+func Codef(code int, format string, a ...any) Error {
+	return Error{
+		Code:    code,
+		Message: fmt.Sprintf(format, a...),
+	}
+}
